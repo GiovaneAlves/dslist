635a23b768dc48d1cf1401864467836a57bbd83a
Giovane Henrique <giovanehenrique7@hotmail.com>
2023-05-09 23:38:20 -0300

    Project Create

---
+++ src/test/java/com/giovaneAlves/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giovaneAlves.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
